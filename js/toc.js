b22a17153e15678d44e72989f02197ddaf891f74
cszhjh <cszhjh@gmail.com>
2022-03-04 12:20:09 +0800

    Site updated: 2022-03-04 12:20:09

--- js/toc.js
+++ js/toc.js
@@ -1,63 +1,63 @@
 function tocInit() {
-  var toc = document.getElementById('post-toc')
-  if (!toc) return
+  var toc = document.getElementById("post-toc");
+  if (!toc) return;
 
-  var menuHeight = 34
-  var startTop = 280
+  var menuHeight = 34;
+  var startTop = 280;
   function tocFixed() {
     if (this.window.scrollY >= menuHeight) {
-      toc.style.position = 'fixed'
-      toc.style.top = menuHeight + 'px'
+      toc.style.position = "fixed";
+      toc.style.top = menuHeight + "px";
     } else {
-      toc.style.position = 'absolute'
-      toc.style.top = startTop + 'px'
+      toc.style.position = "absolute";
+      toc.style.top = startTop + "px";
     }
   }
 
-  var tocLink = document.getElementsByClassName('toc-link')
-  var headerLink = document.getElementsByClassName('headerlink')
-  var headerLinkLength = headerLink.length
-  var headerLinkTop = new Array(headerLinkLength)
-  var currentIndex = 0
+  var tocLink = document.getElementsByClassName("toc-link");
+  var headerLink = document.getElementsByClassName("headerlink");
+  var headerLinkLength = headerLink.length;
+  var headerLinkTop = new Array(headerLinkLength);
+  var currentIndex = 0;
 
-  tocLink[0].className += ' active'
+  tocLink[0].className += " active";
 
   function getOffsetTopByBody(el) {
-    let offsetTop = 0
-    while (el && el.tagName !== 'BODY') {
-      offsetTop += el.offsetTop
-      el = el.offsetParent
+    let offsetTop = 0;
+    while (el && el.tagName !== "BODY") {
+      offsetTop += el.offsetTop;
+      el = el.offsetParent;
     }
-    return offsetTop
+    return offsetTop;
   }
 
   function tocActive() {
     for (var i = 0; i < headerLinkLength; ++i) {
-      headerLinkTop[i] = getOffsetTopByBody(headerLink[i])
+      headerLinkTop[i] = getOffsetTopByBody(headerLink[i]);
     }
 
     for (var i = 0; i < tocLink.length; ++i) {
       if (
-        window.scrollY >= headerLinkTop[i] - 1 &&
+        window.scrollY >= headerLinkTop[i] - 2 &&
         (i + 1 === tocLink.length || window.scrollY < headerLinkTop[i + 1])
       ) {
         for (var j = 0; j < tocLink.length; ++j) {
-          tocLink[j].className = 'toc-link'
+          tocLink[j].className = "toc-link";
         }
         if (currentIndex != i) {
-          toc.scrollBy(0, currentIndex < i ? 30 : -30)
-          currentIndex = i
+          toc.scrollBy(0, currentIndex < i ? 30 : -30);
+          currentIndex = i;
         }
-        tocLink[i].className += ' active'
+        tocLink[i].className += " active";
       }
     }
   }
 
-  window.addEventListener('scroll', function (e) {
-    tocFixed()
-    tocActive()
-  })
+  window.addEventListener("scroll", function (e) {
+    tocFixed();
+    tocActive();
+  });
 }
-window.addEventListener('load', tocInit)
+window.addEventListener("load", tocInit);
 
 // window.addEventListener('load', tocInit)
